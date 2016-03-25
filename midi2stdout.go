b4d82a44280605d3f4c77edf7809d8d066b4c938
Matt Todd <chiology@gmail.com>
2016-03-25 00:22:52 -0700

    Output MIDI messages to STDOUT

--- midi2stdout.go
+++ midi2stdout.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+
+  "github.com/rakyll/portmidi"
+)
+
+type midimsg struct {
+  a, b, c float64
+}
 
 func main() {
-  fmt.Println("midi2stdout")
+  portmidi.Initialize()
+  defer portmidi.Terminate()
+
+  deviceId := portmidi.GetDefaultInputDeviceId()
+
+  in, err := portmidi.NewInputStream(deviceId, 1024)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // or alternatively listen events
+  events := in.Listen()
+
+  for event := range events {
+    fmt.Printf("%x\t%d\t%d\n", event.Status, event.Data1, event.Data2)
+  }
 }
